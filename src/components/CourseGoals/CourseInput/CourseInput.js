84bb61a2a0d97cc897cd26f519f7fadb9ce6e0c7
Mark <mark.nelubin@gmail.com>
2021-07-06 00:27:09 +0300

    added dynamic styles

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,63 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
+
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+  
+
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props => props.inValid ? 'red' : 'black'};
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => props.inValid ? 'red' : '#ccc'};
+//     background: ${props => props.inValid ? 'rgb(255, 181, 173)' : 'transparent'};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 // props {onAddGoal:addGoalHandler} - function, that updates goals array
 const CourseInput = props => {
   // set initial input value empty
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
   // when user enters something update entered value
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
   // when user submit form not reload page and update array with entered value 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0){
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div 
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,41 @@
+// import styled from 'styled-components';
 import React from 'react';
 
-import './Button.css';
+import styles from './Button.module.css';
+
+// attacked template literal, return new button component
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+
 // sended props = {type:"submit"}
 const Button = props => {
-  console.log(props);
   return (
     // use additional props that we receive using button component - children
     // props.children is anything, that we pass in between the opening and closing component tags for Button i.e <Button></Button>
-    <button type={props.type} className="button">
+    <button type={props.type} className={styles.button}>
       {props.children}
     </button>
   );
